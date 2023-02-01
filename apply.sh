43aa7f71e305170129e66562142d8f9d529be7cd
Juno Kim <juno@eng.ucsd.edu>
2023-02-01 15:14:17 -0800

    update apply.sh for zsh

--- apply.sh
+++ apply.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cp vimrc ~/.vimrc
-cp git-completion.bash ~/.git-completion.bash
-cp git-prompt.sh ~/.git-prompt.sh
-cat bashrc >> ~/.bashrc
-source ~/.bashrc
+cp zshrc ~/.zshrc
+source ~/.zshrc
+cp screenrc ~/.screenrc
+
+#cp git-completion.bash ~/.git-completion.bash
+#cp git-prompt.sh ~/.git-prompt.sh
+#cat bashrc >> ~/.bashrc
